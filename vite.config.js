2f8ec31673dec78146bb5aa03411ec40b84a8018
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2025-12-30 22:42:21 +0530

    Installed tailwindcss
    tailwindcss has been added and is confirmed to be working

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(new URL(".", import.meta.url).pathname, "./src"),
+    },
+  },
+
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
+});
